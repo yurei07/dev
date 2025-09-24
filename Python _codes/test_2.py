891e8cf57ddea608e8e5b47928693f1d7a0db49b
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-09-24 21:34:08 +0200

    leetcode

--- Python _codes/test_2.py
+++ Python _codes/test_2.py
@@ -1,8 +1,24 @@
-import time
-helloWolrd = 'Hello World!';
-lis = []
+s = '(]'
 
-for i in helloWolrd:
-    lis.append(i)
-    print(''.join(lis))
-    time.sleep(0.5)
+def isValid(s):
+        a = 0
+
+        for i in s: 
+            if '(' == i:
+                a =+ 1
+            elif ')' == i:
+                a -= 1
+            elif '[' == i:
+                a =+ 2
+            elif ']' == i:
+                a -= 2
+            if '{' == i:
+                a =+ 3
+            elif '}' == i:
+                a -= 3
+    
+            if a > 0:
+                return False
+            else:
+                return True
+
