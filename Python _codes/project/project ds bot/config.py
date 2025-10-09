844a4a8029c54f386c85f32e475f240ebaea36eb
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-10-09 22:31:44 +0200

    update

--- Python _codes/project/project ds bot/config.py
+++ Python _codes/project/project ds bot/config.py
@@ -1,4 +1,4 @@
-DISCORD_TOKEN = "MTM1NjUxNDI1MzY4MjUxMTkzNA.GCy-qj.uOZiXMK34hNrMWgUSTw9gaEx2Xp62O1h-VoMmI"
+DISCORD_TOKEN = "####"
 
 RCLONE_REMOTE = "rclone:myfiles"
 LOCAL_PATH = "/home/nixos/Documents"
