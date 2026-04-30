a9bfc24b25ba6712d09f8d4cf98cfd0583963f83
Prizrak <nazariipalahni@gmial.com>
2026-04-30 15:24:50 +0200

    feat: add utils for micro

--- bash/hello.sh
+++ bash/hello.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # disk_percent=$(command df / | tail -1 | awk '{print $5}' | tr -d '%')
-disk_percent=67
-if [ "$disk_percent" -ge 90 ]; then
-  echo "CRITICAL: disk usage is ${disk_percent}%"
-elif [ "$disk_percent"  -ge 80 ]; then
-  echo "WARNING: disk usage is ${disk_percent}%"
+# disk_percent=67
+# if [ "$disk_percent" -ge 90 ]; then
+#   echo "CRITICAL: disk usage is ${disk_percent}%"
+# elif [ "$disk_percent"  -ge 80 ]; then
+#   echo "WARNING: disk usage is ${disk_percent}%"
+#
+# else 
+#   echo "OK: disk usage is ${disk_percent}%"
+# fi
+#
 
-else 
-  echo "OK: disk usage is ${disk_percent}%"
-fi
+
 
